f58f4b11b92199c6a6dbfdfcd6e59d482ff9c194
abdulwahiid08 <abdulwahid292000@gmail.com>
2024-03-14 14:53:45 +0700

    Feat: Update main app js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
 
+import Navbar from "@/components/layouts/Navbar";
+import MasterApp from "@/components/layouts/MasterApp";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MasterApp>
+      <Component {...pageProps} />
+    </MasterApp>
+  );
 }
