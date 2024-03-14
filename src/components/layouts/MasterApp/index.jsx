547a7c1e5bda7aa1a0ed6ed121f17c30487ea484
abdulwahiid08 <abdulwahid292000@gmail.com>
2024-03-14 14:52:50 +0700

    Feat: add MasterApp Layout

---
+++ src/components/layouts/MasterApp/index.jsx
@@ -0,0 +1,18 @@
+import { Children } from "react";
+import Navbar from "../Navbar";
+import { useRouter } from "next/router";
+
+const MasterApp = (props) => {
+  const { children } = props;
+  const { pathname } = useRouter();
+  const disableNavbar = ["/auth/login", "/auth/register"]; // Disable route jika mengakses route tsb
+
+  return (
+    <main>
+      {!disableNavbar.includes(pathname) && <Navbar />}
+      {children}
+    </main>
+  );
+};
+
+export default MasterApp;
