e2c23d961845c7015d2823d089c011974f264dc2
abdulwahiid08 <abdulwahid292000@gmail.com>
2024-03-15 08:24:52 +0700

    Feat: Refactoring Code Navbar

--- src/components/layouts/Navbar/index.jsx
+++ src/components/layouts/Navbar/index.jsx
@@ -3,6 +3,12 @@ import { useRouter } from "next/router";
 
 const Navbar = () => {
   const router = useRouter();
+  const navbar = [
+    { title: "Dashboard", link: "/" },
+    { title: "About", link: "/about" },
+    { title: "Product", link: "/product" },
+    { title: "Shop", link: "/shop" },
+  ];
   return (
     <nav className="bg-gray-800 sticky top-0 mb-5">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -19,37 +25,17 @@ const Navbar = () => {
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <Link
-                  href="/"
-                  className={`${
-                    router.pathname === "/" ? "bg-gray-900" : ""
-                  }  text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium`}
-                  aria-current="page"
-                >
-                  Dashboard
-                </Link>
-                <Link
-                  href="/about"
-                  className={`${
-                    router.pathname === "/about" ? "bg-gray-900" : ""
-                  } text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium`}
-                >
-                  About
-                </Link>
-                <Link
-                  href="/product"
-                  className={`${
-                    router.pathname === "/product" ? "bg-gray-900" : ""
-                  } text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium`}
-                >
-                  Product
-                </Link>
-                {/* <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Calendar
-                </a> */}
+                {navbar.map((nav) => (
+                  <Link
+                    href={nav.link}
+                    className={`${
+                      router.pathname === nav.link ? "bg-gray-900" : ""
+                    }  text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium`}
+                    aria-current="page"
+                  >
+                    {nav.title}
+                  </Link>
+                ))}
               </div>
             </div>
           </div>
