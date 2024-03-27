42e7026f27d636c7671c7b93052637756da2d28a
abdulwahiid08 <abdulwahid292000@gmail.com>
2024-03-27 14:25:20 +0700

    refactor: update style navbar

--- src/components/layouts/Navbar/index.jsx
+++ src/components/layouts/Navbar/index.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   ];
   return (
     <nav className="bg-gray-800 sticky top-0 mb-5">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
