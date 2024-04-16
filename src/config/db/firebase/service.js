fd908413f2f26d8f2b22f9dfee0b344bc7880b13
abdulwahiid08 <abdulwahid292000@gmail.com>
2024-04-16 14:25:11 +0700

    feat: add api get by id product

--- src/config/db/firebase/service.js
+++ src/config/db/firebase/service.js
@@ -1,9 +1,16 @@
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
 import app from "./init";
 
-// Insialisasi db firebase yang ada di file app
+/* Insialisasi db firebase yang ada di file app */
 const firestore = getFirestore(app);
 
+/* Get All Data */
 export const retriveData = async (fieldDb) => {
   const snapshot = await getDocs(collection(firestore, fieldDb));
 
@@ -12,6 +19,15 @@ export const retriveData = async (fieldDb) => {
     id: doc.id,
     ...doc.data(),
   }));
+
+  return data;
+};
+
+/* Get Data by Id */
+export const retriveDataById = async (fieldDb, id) => {
+  // Jika ambil cuma satu data, maka menggunakan getDoc bukan getDocs dan juga tidak pakai collection tetapi doc
+  const snapshot = await getDoc(doc(firestore, fieldDb, id));
+  const data = snapshot.data();
 
   return data;
 };
