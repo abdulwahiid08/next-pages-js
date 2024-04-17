a66462fc80769fb59850240bc45e2bab5f1175b8
abdulwahiid08 <abdulwahid292000@gmail.com>
2024-04-17 12:08:00 +0700

    refactor: update detail product

--- src/pages/product/[id].jsx
+++ src/pages/product/[id].jsx
@@ -1,11 +1,50 @@
+import initFetcher from "@/config/swr/fetch";
 import DetailProductViews from "@/views/Product/detail";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 
-const ProductDetailPage = () => {
+const ProductDetailPage = ({ product }) => {
+  const req = useRouter();
+
+  /* Client Side Rendering */
+  /* Fecthing data by id */
+  // const { data, error, isLoading } = useSWR(
+  //   `/api/product/${req.query.id}`,
+  //   initFetcher
+  // );
+
+  /* Navigation */
+  const handleButtonBack = () => {
+    req.push("/product"); // Redirect
+  };
+
   return (
     <>
-      <DetailProductViews />
+      {/* Client Side */}
+      {/* <DetailProductViews
+        data={isLoading || data?.data}
+        handleButtonBack={handleButtonBack}
+      /> */}
+
+      {/* Server Side */}
+      <DetailProductViews data={product} handleButtonBack={handleButtonBack} />
     </>
   );
 };
 
 export default ProductDetailPage;
+
+/* Server Side Rendering */
+export const getServerSideProps = async ({ params }) => {
+  const model = await fetch(`http://localhost:3000/api/product/${params.id}`); // Url harus lengkap dengan http//.... dst
+  const data = await model.json();
+
+  // Return sebuah props
+
+  return {
+    props: {
+      product: data.data,
+    },
+  };
+};
